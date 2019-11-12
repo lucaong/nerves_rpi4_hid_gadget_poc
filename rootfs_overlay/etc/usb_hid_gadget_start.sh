8bbb74868d4e5f6de822db53bc0fbf0e346870e8
Luca Ongaro <mail@lucaongaro.eu>
2019-11-12 18:51:33 +0100

    better comments

--- rootfs_overlay/etc/usb_hid_gadget_start.sh
+++ rootfs_overlay/etc/usb_hid_gadget_start.sh
@@ -27,10 +27,5 @@ echo 1 > functions/hid.usb0/subclass
 echo 8 > functions/hid.usb0/report_length
 printf "%b" "\\x05\\x01\\x09\\x06\\xa1\\x01\\x05\\x07\\x19\\xe0\\x29\\xe7\\x15\\x00\\x25\\x01\\x75\\x01\\x95\\x08\\x81\\x02\\x95\\x01\\x75\\x08\\x81\\x03\\x95\\x05\\x75\\x01\\x05\\x08\\x19\\x01\\x29\\x05\\x91\\x02\\x95\\x01\\x75\\x03\\x91\\x03\\x95\\x06\\x75\\x08\\x15\\x00\\x25\\x65\\x05\\x07\\x19\\x00\\x29\\x65\\x81\\x00\\xc0" > functions/hid.usb0/report_desc
 
-# ln -s functions/hid.usb0 configs/c.1/
-# File.ln_s("/sys/kernel/config/usb_gadget/g1/functions/hid.usb0", "/sys/kernel/config/usb_gadget/g1/configs/c.1/hid.usb0")
-# End functions
-
-# ls /sys/class/udc > UDC
-
-# ls /sys/class/udc > /sys/kernel/config/usb_gadget/g1/UDC
+# Before the HID device can be used, we still have to symlink the fuctions to
+# the configuration and activate the UDC. This is done upon application start.
